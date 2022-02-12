27863c58114c847ede8ef6b7dc7c13883f5a4e0e
Tomasz Krajewski <krajaneczek@gmail.com>
2022-02-12 12:05:53 +0100

    wadd tests

--- src/components/CurrencyForm/CurrentyForm.test.js
+++ src/components/CurrencyForm/CurrentyForm.test.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import CurrencyForm from './CurrencyForm';
 import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
@@ -8,30 +8,32 @@ describe('Component CurrencyForm', () => {
     render(<CurrencyForm action={() => {}} />);
   });
   it('should run action callback with proper data on form submit', () => {
-    const action = jest.fn();
 
+    const testCases = [
+      { amount: '100', from: 'PLN', to: 'USD' },
+      { amount: '20', from: 'USD', to: 'PLN' },
+      { amount: '200', from: 'PLN', to: 'USD' },
+      { amount: '345', from: 'USD', to: 'PLN' },
+];
 
-    // render component
-    render(<CurrencyForm action={action} />);
+const action = jest.fn();
 
-    // find “convert” button
-    const submitButton = screen.getByText('Convert');
+for(const testObj of testCases) {
+  render(<CurrencyForm action={action} />);
+  
+  const testAmount = screen.getByTestId('testAmount');
+  const testFrom = screen.getByTestId('testFrom');
+  const testTo = screen.getByTestId('testTo');
+  userEvent.type(testAmount, testObj.amount);
+  userEvent.selectOptions(testFrom, testObj.from);
+  userEvent.selectOptions(testTo, testObj.to);
 
-    // simulate user click on "convert" button
-    userEvent.click(submitButton);
-    
-    // find fields elements
-    const testAmount = screen.getByTestId('testAmount');
-    const testFrom = screen.getByTestId('testFrom');
-    const testTo = screen.getByTestId('testTo');
-
-    // set test values to fields
-    userEvent.type(testAmount, '100');
-    userEvent.selectOptions(testFrom, 'PLN');
-    userEvent.selectOptions(testTo, 'USD');
-
-    // check if action callback was called once and with proper argument
-    expect(action).toHaveBeenCalledTimes(1);
-    expect(action).toHaveBeenCalledWith({ amount: 100, from: 'PLN', to: 'USD' });
+  
+  const submitButton = screen.getByText('Convert');
+  userEvent.click(submitButton);
+  expect(action).toHaveBeenCalledWith({ amount: parseInt(testObj.amount), from: testObj.from, to: testObj.to });
+  cleanup();
+}
+expect(action).toHaveBeenCalledTimes(4);
   });
 });
